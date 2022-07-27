4ac2bfd8cfd839e0fa62929ed0d5084ca40c90ac
liriolebron <liriolebron1@gmail.com>
2022-07-26 20:15:43 -0400

    Initial commit

---
+++ UltimateParcelQuoter/DTOs/DHLService/PackateQuoteDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace UltimateParcelQuoter.DTOs.DHLService
+{
+    public class PackateQuoteDTO
+    {
+        public string ContactAdress { get; set; }
+        public string WarehouseAddress { get; set; }
+    }
+}
+ 
